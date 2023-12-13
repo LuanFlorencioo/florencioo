0548f5ef8bff68bd90b61c7db32c0b6342b23a80
LuanFlorencio <luanflorencio.dev@gmail.com>
2023-12-13 00:11:19 -0300

    feat(Home): add HomeHero and HomeGreeting in Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,23 @@
+import { HomeGreeting, HomeHero, HomeTypewriter } from '@/components';
+
 export default function Home() {
   return (
-    <></>
+    <main className='w-full min-h-screen relative'>
+      <HomeHero />
+
+      <HomeGreeting>
+        <HomeGreeting.Title>
+          Desenvolvedor Web 
+          <HomeTypewriter
+            words={['Front End.', 'Back End.', 'Full Stack.']}
+            loop={true}
+          />
+        </HomeGreeting.Title>
+
+        <HomeGreeting.Text>
+          Transformando Ideias em Aplicações Web com Criatividade, Inovação e Paixão.
+        </HomeGreeting.Text>
+      </HomeGreeting>
+    </main>
   )
 }
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,13 @@
 import Header from './Header';
 import Footer from './Footer';
+import HomeHero from './HomeHero';
+import HomeTypewriter from './HomeTypewriter';
+import { HomeGreeting } from './HomeGreeting';
 
 export {
   Header,
   Footer,
+  HomeHero,
+  HomeTypewriter,
+  HomeGreeting,
 }
