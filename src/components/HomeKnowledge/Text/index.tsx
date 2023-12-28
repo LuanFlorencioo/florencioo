7cb87ee62b2a7a6f66ba8b2bbb7b5ac3a4158874
LuanFlorencio <luanflorencio.dev@gmail.com>
2023-12-28 13:41:37 -0300

    feat(HomeKnowledge): create HomeKnowledge component

---
+++ src/components/HomeKnowledge/Text/index.tsx
@@ -0,0 +1,11 @@
+import { ComponentProps } from 'react';
+
+type TextProps = ComponentProps<'p'>;
+
+export default function Text({ children, ...props }: TextProps) {
+  return (
+    <p {...props} className='text-center font-normal text-lg text-brand-100 selection:bg-brand-200 selection:text-brand-500'>
+      {children}
+    </p>
+  )
+}
---
+++ src/components/HomeKnowledge/index.ts
@@ -0,0 +1,10 @@
+import Root from './Root';
+import Title from './Title';
+import Text from './Text';
+import CardList from './CardList';
+
+export const HomeKnowledge = Object.assign(Root, {
+  Title,
+  Text,
+  CardList,
+})
