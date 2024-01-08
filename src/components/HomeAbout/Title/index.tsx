6deb39e64107eb1cd0be72d4f1226fba13312f95
LuanFlorencio <luanflorencio.dev@gmail.com>
2024-01-08 11:47:03 -0300

    feat(HomeAbout): add animation in Title component

--- src/components/HomeAbout/Title/index.tsx
+++ src/components/HomeAbout/Title/index.tsx
@@ -1,11 +1,18 @@
-import { ComponentProps } from 'react';
+'use client';
 
-type TitleProps = ComponentProps<'h2'>
+import { ComponentProps } from 'react';
+import { motion } from 'framer-motion';
+
+type TitleProps = ComponentProps<typeof motion.h2>
 
 export default function Title({ children, ...props }: TitleProps) {
   return (
-    <h2 {...props} className='font-bold text-3xl text-brand-500'>
+    <motion.h2 {...props} className='font-bold text-3xl text-brand-500'
+      initial={{ opacity: 0, translateX: -50 }}
+      whileInView={{ opacity: 1, translateX: 0 }}
+      transition={{ duration: .4 }}
+    >
       {children}
-    </h2>
+    </motion.h2>
   )
 }
