eaec25381f9660a2a2a63740d67b045435400dcd
LuanFlorencio <luanflorencio.dev@gmail.com>
2024-01-08 11:49:00 -0300

    feat(HomeAbout): add animation in Text component

--- src/components/HomeAbout/Text/index.tsx
+++ src/components/HomeAbout/Text/index.tsx
@@ -1,11 +1,19 @@
-import { ComponentProps } from 'react';
+'use client';
 
-type TextProps = ComponentProps<'p'>
+import { ComponentProps } from 'react';
+import { motion } from 'framer-motion';
+
+type TextProps = ComponentProps<typeof motion.p>
 
 export default function Text({ children, ...props }: TextProps) {
   return (
-    <p {...props} className='font-normal text-base text-brand-500'>
+    <motion.p {...props} className='font-normal text-base text-brand-500'
+      initial={{ opacity: 0, translateX: -50 }}
+      whileInView={{ opacity: 1, translateX: 0 }}
+      exit={{ opacity: 0, translateX: -50 }}
+      transition={{ duration: .4 }}
+    >
       {children}
-    </p>
+    </motion.p>
   )
 }
