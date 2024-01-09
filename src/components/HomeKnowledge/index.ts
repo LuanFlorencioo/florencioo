90f5112053700fea71513cc64f8bcbb0cd4001c9
LuanFlorencio <luanflorencio.dev@gmail.com>
2024-01-09 17:26:12 -0300

    feat(HomeKnowledge): create SkillsContainer component

--- src/components/HomeKnowledge/index.ts
+++ src/components/HomeKnowledge/index.ts
@@ -2,9 +2,11 @@ import Root from './Root';
 import Title from './Title';
 import Text from './Text';
 import CardList from './CardList';
+import SkillsContainer from './SkillsContainer';
 
 export const HomeKnowledge = Object.assign(Root, {
   Title,
   Text,
   CardList,
+  SkillsContainer,
 })
