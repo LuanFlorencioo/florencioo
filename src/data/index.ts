70a57c331b2c2312a697c0a6ae37d18dab0089c8
LuanFlorencio <luanflorencio.dev@gmail.com>
2024-01-09 17:25:16 -0300

    feat(SkillBadge): create SkillBadge component

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,9 +1,13 @@
 import { pages } from './pages';
 import type { Page } from './pages';
 import { knowledges } from './knowledges';
+import type { Skill } from './skills';
+import { skills } from './skills';
 
 export {
   pages,
   Page,
   knowledges,
+  skills,
+  Skill,
 }
