d3f51962a838a1b57160170d12bed0c358427122
LuanFlorencio <luanflorencio.dev@gmail.com>
2024-01-30 12:33:29 -0300

    feat(Button): update link target button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,14 +1,15 @@
-import { ComponentProps } from 'react';
+import { ComponentProps, HTMLAttributeAnchorTarget } from 'react';
 import Link from 'next/link';
 
 type ButtonProps = ComponentProps<'button'> & {
   href?: string;
+  target?: HTMLAttributeAnchorTarget;
 };
 
-export default function Button({ children, href, ...props }: ButtonProps) {
+export default function Button({ children, href, target, ...props }: ButtonProps) {
   return href
     ? (
-      <Link href={href} className='w-max h-max'>
+      <Link href={href} target={target} className='w-max h-max'>
         <button {...props} className='w-max h-10 px-8 rounded-lg bg-gradient-to-b from-brand-300 to-brand-200 flex justify-center items-center gap-2 text-base text-brand-500 select-none transition md:hover:-translate-y-1 md:hover:shadow-xl md:hover:shadow-[#0099FF25]'>
           {children}
         </button>
