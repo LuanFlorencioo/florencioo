8f13d2ec8ff738b29c3bceead3140da24ac343b3
LuanFlorencio <luanflorencio.dev@gmail.com>
2024-02-29 21:37:03 -0300

    feat(Header): adjust Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,11 @@
-import HeaderTitle from './HeaderTitle';
+import { FlorenciooLogo } from '@/components';
 import Navbar from './Navbar';
 
 export default function Header() {
   return (
     <header className='w-full px-4 py-3 fixed top-0 left-0 z-50 bg-brand-500 border-b border-[#00000010] animate-header opacity-0'>
       <div className='max-w-default mx-auto flex justify-between items-center select-none'>
-        <HeaderTitle>
-          Florencioo
-        </HeaderTitle>
+        <FlorenciooLogo role='main' />
 
         <Navbar />
       </div>
