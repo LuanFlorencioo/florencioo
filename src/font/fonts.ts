62b1250aa9d97db301bc8387ceb9446b2d463597
LuanFlorencio <luanflorencio.dev@gmail.com>
2025-02-27 22:09:58 -0300

    feat(font): remove depreciated font

--- src/font/fonts.ts
+++ src/font/fonts.ts
@@ -1,4 +1,4 @@
-import { Geist, Geist_Mono, Nova_Flat } from "next/font/google";
+import { Geist, Geist_Mono } from "next/font/google";
 
 const geistSans = Geist({
 	subsets: ["latin"],
@@ -10,12 +10,4 @@ const geistMono = Geist_Mono({
 	variable: "--font-geist-mono",
 });
 
-const novaFlat = Nova_Flat({
-	subsets: ["latin"],
-	weight: "400",
-	variable: "--font-nova-flat",
-});
-
-export const fonts = [geistSans, geistMono, novaFlat]
-	.map((f) => f.variable)
-	.join(" ");
+export const fonts = [geistSans, geistMono].map((f) => f.variable).join(" ");
