596799a1e4a2e0aa8be86d8da00397ea21d57dec
LuanFlorencio <luanflorencio.dev@gmail.com>
2025-03-21 20:06:05 -0300

    feat(component): update ProjectCard component

--- src/components/project-card/ProjectCard.tsx
+++ src/components/project-card/ProjectCard.tsx
@@ -1,21 +1,51 @@
+import { Button } from "@/ui/button";
+import { Popover, PopoverContent, PopoverTrigger } from "@/ui/popover";
+import { SiGithub } from "@icons-pack/react-simple-icons";
+import { SquareArrowOutUpRight } from "lucide-react";
 import Link from "next/link";
 
 export function ProjectCard({
 	children,
-	href,
+	appUrl,
+	description,
+	repoUrl,
 }: {
 	children: React.ReactNode;
-	href: string;
+	repoUrl: string;
+	description: string;
+	appUrl?: string;
 }) {
 	return (
-		<Link
-			href={href}
-			target="_blank"
-			className="w-full max-w-[300px] h-[200px] lg:max-w-[400px] lg:h-[250px]"
-		>
-			<div className="w-full h-full relative cursor-pointer transition-all group">
-				{children}
-			</div>
-		</Link>
+		<Popover>
+			<PopoverTrigger asChild>
+				<div className="w-full max-w-[300px] h-[200px] lg:max-w-[400px] lg:h-[250px] relative cursor-pointer transition-all group">
+					{children}
+				</div>
+			</PopoverTrigger>
+
+			<PopoverContent>
+				<div className="flex flex-col gap-4">
+					<p className="text-sm text-muted-foreground line-clamp-4">
+						{description}
+					</p>
+
+					<div className="flex items-center gap-4">
+						{appUrl && (
+							<Link href={appUrl} target="_blank">
+								<Button>
+									<SquareArrowOutUpRight /> Ver Projeto
+								</Button>
+							</Link>
+						)}
+
+						<Link href={repoUrl} target="_blank">
+							<Button variant="secondary">
+								<SiGithub /> Repo
+							</Button>
+						</Link>
+					</div>
+				</div>
+			</PopoverContent>
+		</Popover>
 	);
 }
